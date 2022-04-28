a392f60a3bf3cf45792386f7d934d7e33277aba5
Ilhad <ilhadmammadli@gmail.com>
2022-04-28 14:48:11 +0400

    bacgground made responsive

--- components/BackgroundImage.vue
+++ components/BackgroundImage.vue
@@ -18,8 +18,9 @@
 
         <div class="description">
           <p class="descriptionTitle">
-            Lorem Ipsum is simply dummy text of the printing and <br/> typesetting
-            industry. Lorem Ipsum has been the industry's <br/> standard.
+            Lorem Ipsum is simply dummy text of the printing and <br />
+            typesetting industry. Lorem Ipsum has been the industry's <br />
+            standard.
           </p>
         </div>
         <div class="details">
@@ -32,10 +33,8 @@
 </template>
 
 <style>
-
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,300&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,300&display=swap");
 
 .headerBackground {
   position: relative;
@@ -97,40 +96,176 @@
   font-family: "Roboto", sans-serif;
 }
 
-.slogan { 
+.slogan {
   margin-top: 20px;
 }
 
-.sloganText { 
+.sloganText {
   color: #000000;
   font-size: 50px;
-  font-family: 'Poppins', sans-serif;
-
+  font-family: "Poppins", sans-serif;
 }
 
-.desctiption { 
+.desctiption {
   margin-top: 20px;
 }
 
-.descriptionTitle { 
+.descriptionTitle {
   color: #000000;
   font-size: 16px;
-  font-family: 'Poppins';
+  font-family: "Poppins";
   font-weight: lighter;
 }
 
-.details { 
+.details {
   margin-top: 30px;
+}
+
+.getDetails {
+  background-color: #fa8305;
+  color: #ffffff;
+  font-size: 16px;
+  width: 195px;
+  height: 53px;
+  border-radius: 40px;
+}
+
+@media (max-width: 1500px) {
+  .headerFrontRight {
+    height: 470px;
+  }
+
+  .headerFront {
+    top: -150px;
+  }
+
+  .footerBackground {
+    margin-top: 250px;
+    height: 446px;
+  }
+}
+
+@media (max-width: 1200px) {
+  .headerFrontRight {
+    height: 370px;
+    margin-top: -60px;
+  }
+
+  .headerFront {
+    top: -200px;
+  }
+
+  .footerBackground {
+    margin-top: 150px;
+    height: 356px;
+  }
+
+  .headerFrontLeft {
+    margin-top: -60px;
+  }
+}
+
+@media (max-width: 1100px) {
+
+.headerFrontLeft {
+    margin-top: -20px;
+  }
+
+.sloganText {
+  font-size: 40px;
+
+}
+
+.descriptionTitle {
+  font-size: 13px;
+}
+
+.headerFrontRight { 
+  margin-top: 10px;
+}
+
+}
+
+@media (max-width: 800px) {
+  .sloganText {
+  font-size: 25px;
+
+}
+
+.footerBackground {
+    margin-top: 150px;
+    height: 256px;
+  }
+
+.descriptionTitle {
+  margin-top: 20px;
+  font-size: 10px;
+}
+
+.headerFrontRight {
+    height: 270px;
+    margin-top: 20px;
+  }
+
+  .headerFrontLeft { 
+    margin-top: -40px;
+  }
+
+}
+
+@media (max-width: 800px) {
+  .sloganText {
+  font-size: 20px;
+
+}
+
+.descriptionTitle {
+  margin-top: 20px;
+  font-size: 9px;
+}
+}
+
+@media (max-width: 425px) { 
+  .headerFrontRight { 
+    display: none;
+  }
+
+.headerFront { 
+  justify-content: center;
   
 }
 
+.headerFrontLeft { 
+  width: 100%;
+  left: auto;
+
+}
+
+.headTitle { 
+  margin-top: -20px;
+  
+}
+
+}
+
+@media (max-width: 325px) {
+
+  .headTitle { 
+    font-size: 18px;
+  }
+  .sloganText {
+  font-size: 16px;
+
+}
+
+.title { 
+  margin-top: 10px;
+  font-size: 18px;
+}
+
 .getDetails { 
-  background-color: #FA8305;
-  color: #ffffff;
-  font-size: 16px ;
-  width: 195px;;
-  height: 53px;
-  border-radius: 40px;
+  margin-top: -25px;
+}
 }
 </style>
 
